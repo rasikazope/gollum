74f6650b9d9e83d6b97cdb4d5ea4d54eae22a729
rasikazope <rasikaz@gmail.com>
2017-08-29 16:12:39 -0700

    magic carpet overalpping interval

---
+++ ik/array/overlapping_intervals_pratice.cpp
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <algorithm>
+#include <stack>
+#include <iostream>
+
+using namespace std;
+
+struct interval {
+    int start;
+    int end;
+};
+
+
+bool
+inter_comp(interval i1, interval i2)
+{
+    if (i1.start < i2.start) {
+        return true;
+    }
+    return false;
+}
+
+
+void merge_intervals(struct interval intervals[], int size)
+{
+
+    sort(intervals, intervals + size, inter_comp);
+    stack<struct interval> mystack;
+    
+    for (int i = 0; i < size; i++) {
+        if (mystack.empty()) {
+            mystack.push(intervals[i]);
+        } else {
+            struct interval top = mystack.top();
+            if (top.end >= intervals[i].start) {
+                mystack.pop();
+                top.end = intervals[i].end;
+                mystack.push(top);
+            } else {
+                mystack.push(intervals[i]);
+            }
+        }
+    }
+    while (!mystack.empty()) {
+       interval top = mystack.top();
+       cout << top.start << " " << top.end << endl;
+       mystack.pop();
+    }
+
+}
+
+
+int main() {
+    //struct interval intervals[] =  {{1,3}, {2,4}, {5,7}, {6,8}};
+    struct interval intervals[] =  {{2,4}, {1,2}};
+    int size = sizeof(intervals)/sizeof(intervals[0]);
+    merge_intervals(intervals, size);
+    return 0;
+}
